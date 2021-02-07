ee5a82a6cf3f6a29c334445928ef85dd78b3c032
Feekra Baset Baishakhi <feekrabaset@gmail.com>
2021-02-07 12:09:08 +0600

    even number found

---
+++ labtask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class labtask
+{ }
+namespace ConsoleApp1
+{
+    class lab
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            for (n = 1; n <= 20; n++)
+            {
+                if (n % 2 == 0)
+
+                    Console.WriteLine("Even Numbers :{0} ", n);
+
+            }
+        }
+
+    }
+}
