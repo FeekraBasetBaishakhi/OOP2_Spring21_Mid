9e79662b5001b47e201fc6de57d9bac8f6ed1c68
Feekra Baset Baishakhi <feekrabaset@gmail.com>
2021-02-07 13:56:26 +0600

    student

---
+++ Student/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTask
+{
+    internal class Student
+    {
+        string name;
+        int id;
+        string depertment;
+        float cgpa;
+
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student();
+            Student s2 = new Student("Feekra Baset Baishakhi",18390103,"CSE",3.39f );
+            s2.ShowDetails();
+
+        }
+        public Student()
+        {
+            Console.WriteLine("Default Constructor");
+        }
+        public Student(string name,int id,string depertment,float cgpa)
+        {
+            Console.WriteLine("Parameterized Constructor");
+            this.name = name;
+            this.id = id;
+            this.depertment = depertment;
+            this.cgpa = cgpa;
+        }
+       
+       
+        
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Depertment: " + depertment);
+            Console.WriteLine("cgpa: " + cgpa);
+
+
+
+        } 
+ 
+    
+    
+    }
+        }
+ 
---
+++ lab task 2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace lab_task_2._1 { 
+private class Student
+{
+        string name;
+        string id;
+        string depertment;
+        float cgpa;
+
+public void Setname (string name)
+        {
+            this.name = name;
+        }   
+        
+   internal void ShowDetails()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("ID: " +id);
+            Console.WriteLine("depertment: " + depertment);
+            Console.WriteLine("CGPA: " + cgpa);
+        }
+    {
+        
+    }
+}
+}
