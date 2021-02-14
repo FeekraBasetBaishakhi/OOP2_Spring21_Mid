c3e19e68e6c5bebb26b638effd512d95dccfcd28
Feekra Baset Baishakhi <feekrabaset@gmail.com>
2021-02-14 13:58:34 +0600

    Array Problems

---
+++ Array/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arr
+{
+    class Program
+    {
+        static void Main(string[] s)
+
+        {
+            Console.WriteLine("Original Array:");
+            int[] arr1 = new int[] { 1, 2 };
+            int[] arr2 = new int[2];
+            foreach (int value in arr1)
+            {
+                Console.WriteLine(value);
+            }
+
+            Array.Copy(arr1, arr2, 2);
+
+            Console.WriteLine("Copied Array:");
+            foreach (int value in arr2)
+            {
+                Console.WriteLine(value);
+            }
+
+            int[] array = { 2, 2, 2, 6, 7 };
+            int count = 1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i; j < array.Length - 1; j++)
+                {
+
+                    if (array[j] == array[j + 1])
+                        count = count + 1;
+                }
+                Console.WriteLine("Number:" + array[i] + " Duplicated=" + count + "times");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Arrray2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrray2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 7, 7, 7, 6, 4, 4, 3, 3, 1 };
+            foreach (int i in arr.Distinct())
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
